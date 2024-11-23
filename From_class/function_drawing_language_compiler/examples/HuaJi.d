b28d5da5d6f768d8feccc9a91833dfd3dfc1f58d
yjrszcq <yjr217@qq.com>
2024-11-23 18:37:11 +0800

    Add sql to 'travel_reservation_system'

---
+++ From_class/function_drawing_language_compiler/examples/HuaJi.d
@@ -0,0 +1,158 @@
+-- 脸
+
+origin is (200, 200);
+scale is (200, 200);
+color is yellow;
+pixsize is 2;
+for t from 0 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+-- 嘴
+
+origin is (200, 220);
+scale is (165, 130);
+color is red;
+pixsize is 2;
+for t from 0 to pi step pi/10000 draw(cos(t), sin(t));
+
+origin is (200, 220);
+scale is (165, 120);
+color is red;
+pixsize is 2;
+for t from 0 to pi step pi/10000 draw(cos(t), sin(t));
+
+-- 红晕
+origin is (65, 160);
+scale is (45, 20);
+color is (255,200, 200);
+pixsize is 2;
+for t from 0 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+origin is (335, 160);
+scale is (45, 20);
+color is (255,200, 200);
+pixsize is 2;
+for t from 0 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+-- 眼睛
+
+origin is (13, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*1/4 to pi*11/8 step pi/10000 draw(cos(t), sin(t));
+
+origin is (137, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*13/8 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+origin is (13, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*1/4 to pi*11/8 step pi/10000 draw(cos(t), sin(t));
+
+origin is (137, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from 0 to pi*3/4 step pi/10000 draw(cos(t), sin(t));
+
+origin is (137, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*13/8 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+origin is (25, 115);
+scale is (12, 12);
+color is (165, 42, 42);
+pixsize is 2;
+for t from 0 to pi*2 step pi/10000 draw(cos(t), sin(t));
+
+origin is (263, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*1/4 to pi*11/8 step pi/10000 draw(cos(t), sin(t));
+
+origin is (387, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from 0 to pi*3/4 step pi/10000 draw(cos(t), sin(t));
+
+origin is (387, 123);
+scale is (14, 14);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*13/8 to 2*pi step pi/10000 draw(cos(t), sin(t));
+
+origin is (275, 115);
+scale is (12, 12);
+color is (165, 42, 42);
+pixsize is 2;
+for t from 0 to pi*2 step pi/10000 draw(cos(t), sin(t));
+
+origin is (75, 140);
+scale is (55, 20);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*11/10 to pi*19/10 step pi/10000 draw(cos(t), sin(t));
+
+origin is (75, 140);
+scale is (85, 50);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*12/10 to pi*18/10 step pi/10000 draw(cos(t), sin(t));
+
+origin is (325, 140);
+scale is (55, 20);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*11/10 to pi*19/10 step pi/10000 draw(cos(t), sin(t));
+
+origin is (325, 140);
+scale is (85, 50);
+color is (255,168, 0);
+pixsize is 2;
+for t from pi*12/10 to pi*18/10 step pi/10000 draw(cos(t), sin(t));
+
+-- 眉毛
+
+origin is (95, 50);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from pi*35/32 to pi*2 step pi/10000 draw(cos(t), sin(t));
+
+origin is (95, 50);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from 0 to pi*5/32 step pi/10000 draw(cos(t), sin(t));
+
+origin is (91, 60);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from pi*19/16 to pi*2 step pi/10000 draw(cos(t), sin(t));
+
+origin is (91, 60);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from 0 to pi*1/16 step pi/10000 draw(cos(t), sin(t));
+
+origin is (305, 50);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from pi*27/32 to pi*61/32 step pi/10000 draw(cos(t), sin(t));
+
+origin is (309, 60);
+scale is (35, 35);
+color is black;
+pixsize is 2;
+for t from pi*15/16 to pi*29/16 step pi/10000 draw(cos(t), sin(t));
